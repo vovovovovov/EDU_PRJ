d2f25f7e660311c29f59603ffeeb849f7f609d35
vovovovovov <2394195869@qq.com>
2024-09-12 17:48:05 +0800

    代码重构

--- roberta-gat/llamaCpp.bat
+++
@@ -1,4 +0,0 @@
-set "CMAKE_ARGS=-DLLAMA_CUBLAS=on CUDACXX=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.3\bin\nvcc.exe -DCUDAToolkit_ROOT=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.3\ -DCUDAToolkit_INCLUDE_DIR=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.3\include -DCUDAToolkit_LIBRARY_DIR=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.3\lib -DCMAKE_CUDA_ARCHITECTURES=native"
-pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir --verbose
-
-
